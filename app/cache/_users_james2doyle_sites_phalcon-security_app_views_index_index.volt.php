1ca0efb5fba004b189e204b0d2ef234f8e8459cf
james2doyle <james2doyle@gmail.com>
2015-06-22 11:52:38 -0400

    remove cached views

--- app/cache/_users_james2doyle_sites_phalcon-security_app_views_index_index.volt.php
+++
@@ -1,5 +0,0 @@
-<h1>Congratulations!</h1>
-
-<p>You're now flying with Phalcon. Great things are about to happen!</p>
-
-<p><a href="session">Login</a> or open the <a href="users">users</a> area</p>
--- app/cache/_users_james2doyle_sites_phalcon-security_app_views_session_success.volt.php
+++
@@ -1,3 +0,0 @@
-<h1>Success!</h1>
-
-<p>You passed the login check!</p>
